ca1aeca3eaf3b2f532fde7fd0731b41b73c2a4bc
CARL MARTIN <cj.martin777@gmail.com>
2020-07-21 23:31:36 -0500

    addded the modals to the first row on the calander

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-const $ = (elm)=>{ 
-    return document.getElementById(elm); 
-   }
+
+
+
+// function Year() {
+    const nd = new Date();
+    const y = nd.getFullYear();
+    const m = nd.getMonth();
+    document.getElementById("cYear").innerHTML =" Year:" + y 
+    + " " + "Month:" + m;
+    console.log("year:" + y + " " + "month:" + m);
+// };
+
+function add() {
+   const m1 = document.getElementById("modal1")
+    if (m1.style.display === "none" ) {
+        m1.style.display = "block"
+    }
+};
+
+function change() {
+   const i = document.getElementById("input");
+   const m2 = document.getElementById("modal2");
+
+    i.select();
+    i.setSelectionRange(0, 100)
+    document.execCommand("copy");
+    
+    console.log("New Item Added:" + i.value);
+
+};
+
+
+
 
    
